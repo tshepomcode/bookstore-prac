ca48d7c97c985c3042cf8f9940245ce25ac76360
tshepomcode <tshepomcode@gmail.com>
2024-04-12 11:07:08 +0200

    ch14

--- books/models.py
+++ books/models.py
@@ -10,6 +10,11 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to="covers/", blank=True)
+    
+    class Meta:
+        permissions = {
+            ("special_status", "Can read all books")
+        }
 
     def __str__(self):
         return self.title
